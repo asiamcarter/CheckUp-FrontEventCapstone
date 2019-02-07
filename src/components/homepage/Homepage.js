c1d41d2b376eccc3723009987c401060712cdf04
Asia Carter <asiamcarter@gmail.com>
2019-02-07 15:15:51 -0600

    adding conditional logic to homepage render

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -10,8 +10,8 @@ export default class Homepage extends Component {
     }
 
     componentDidMount() {
-        DataManager.getAllUserInfo().then(allUsers => this.setState({users: allUsers, dataLoaded: true}))
-      }
+        DataManager.getAllUserInfo().then(allUsers => this.setState({ users: allUsers, dataLoaded: true }))
+    }
 
 
     welcomeUser() {
@@ -21,36 +21,74 @@ export default class Homepage extends Component {
             console.log("find method user id:", user.id)
             return (seshUser === user.id)
         })
-        console.log(userObject)
-        return (
-            <div>
-            <h3>Welcome to your homepage, {userObject.name}!</h3>
-            <br />
-            <div>
-            <Link to={"/appointments"}> <h4> Appointment: </h4></Link>
-
-                <p>{userObject.appointments[0].reason}</p>
-                <p>{userObject.appointments[0].date}</p>
-                <p>{userObject.appointments[0].time}</p>
-            </div>
-            <div>
-            <Link to={"/meds"}>  <h4>Next Medication:</h4></Link>
-            <p>{userObject.medications[0].name} </p>
-            <p>{userObject.medications[0].quantity}</p>
-            <p>{userObject.medications[0].date}</p>
-            </div>
-            </div>
+        if (userObject.appointments.length > 0 && userObject.medications.length > 0) {
+            return (
+                <div>
+                    <h3>Welcome to your homepage, {userObject.name}!</h3>
+                    <br />
+                    <div>
+                        <Link to={"/appointments"}> <h4> Appointment: </h4></Link>
+                        <p>{userObject.appointments[0].reason}</p>
+                        <p>{userObject.appointments[0].date}</p>
+                        <p>{userObject.appointments[0].time}</p>
+                    </div>
+                    <div>
+                        <Link to={"/meds"}><h4>Next Medication:</h4></Link>
+                        <p>{userObject.medications[0].name} </p>
+                        <p>{userObject.medications[0].quantity}</p>
+                        <p>{userObject.medications[0].date}</p>
+                    </div>
+                </div>
             )
-}
+
+        } else if (userObject.appointments.length > 0) {
+            return (
+                <div>
+                    <h3>Welcome to your homepage, {userObject.name}!</h3>
+                    <br />
+                    <div>
+                        <Link to={"/appointments"}> <h4> Appointment: </h4></Link>
+                        <p>{userObject.appointments[0].reason}</p>
+                        <p>{userObject.appointments[0].date}</p>
+                        <p>{userObject.appointments[0].time}</p>
+                    </div>
+                </div>
+
+            )
+        } else if (userObject.medications.length > 0) {
+            return (
+                <div>
+                    <h3>Welcome to your homepage, {userObject.name}!</h3>
+                    <br />
+                    <div>
+                        <Link to={"/meds"}><h4>Next Medication:</h4></Link>
+                        <p>{userObject.medications[0].name} </p>
+                        <p>{userObject.medications[0].quantity}</p>
+                        <p>{userObject.medications[0].date}</p>
+                    </div>
+                </div>
+            )
+
+        } else {
+            return (
+                <div>
+                    <h3>Welcome to your homepage, {userObject.name}!</h3>
+                    <br />
+                </div>
+            )
+        }
+
+
+    }
 
 
     render() {
         console.log(this.state)
-      if (this.state.dataLoaded === false) {
-        return (
-            <>
-            </>
-        )
+        if (this.state.dataLoaded === false) {
+            return (
+                <>
+                </>
+            )
         } else if (this.state.dataLoaded === true) {
             return (
                 <>
